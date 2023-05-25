2111f27fd3c36c38ec9c5f07b44079c94bf7085c
Brian <brian_@DESKTOP-GJH2BCP>
2023-05-25 19:08:34 -0400

    Third commit

--- problems/person.js
+++ problems/person.js
@@ -3,9 +3,36 @@ class Person {
     this.name = name
     this.age = age
   }
+  static greetAll(arr) {
+    return arr.map(el => el.sayHello())
+  }
   sayHello() {
     return `Hello ${this.name}!`
   }
+  visit(person) {
+    return `${this.name} visited ${person.name}`
+  }
+  switchVisit(otherPerson) {
+    return `${otherPerson.name} visited ${this.name}`
+  }
+  update(obj) {
+    if (typeof obj !== 'object') throw new TypeError('Input is not an object!')
+    if (!obj.name || !obj.age) throw new TypeError('Object does not match!')
+    this.name = obj.name
+    this.age = obj.age
+    return this
+
+  }
+  tryUpdate(obj){
+    try {
+      this.update(obj)
+      return true
+    }
+    catch (e) {
+      return false
+    }
+  }
+
 }
 
 module.exports = Person;
--- test/person-spec.js
+++ test/person-spec.js
@@ -4,6 +4,7 @@ const Person = require('../problems/person')
 
 describe('Person', function () {
     const bob = new Person('bob', 20)
+    const sally = new Person('sally', 25)
     it('New instance should have name & age properties', function () {
         expect(bob.name).exist
         expect(bob.name).equal('bob')
@@ -12,5 +13,50 @@ describe('Person', function () {
     })
     it('`sayHello` should greet the Person', function () {
         expect(bob.sayHello()).equal('Hello bob!')
+    })
+    it('`visit` should return a string including argument name', function () {
+        expect(bob.visit(sally)).equal('bob visited sally')
+    })
+    it('`switchVisit` should return a string including argument name', function () {
+        expect(bob.switchVisit(sally)).equal('sally visited bob')
+    })
+})
+describe('update(obj)', function () {
+    const bob = new Person('bob', 20)
+    const sally = new Person('sally', 25)
+    context('Invalid Inputs', function () {
+       it('should not accept non-objects as arguments', function () {
+        expect(() => bob.update(5)).throw(TypeError)
+       })
+       it('should not accept arguments without name or age values', function () {
+        expect(() => bob.update({})).throw(TypeError)
+       })
+    })
+    context('Valid Inputs', function () {
+        it('should update with new values', function () {
+            bob.update(sally)
+            expect(bob.name).to.equal('sally')
+            expect(bob.age).to.equal(25)
+        })
+    })
+
+})
+describe('tryUpdate', function () {
+    const bob = new Person('bob', 20)
+    const sally = new Person('sally', 25)
+    it('should return true if invoked successfully', function () {
+        expect(bob.tryUpdate(sally)).equal(true)
+    })
+    it('should return false if it is not invoked successfully', function () {
+        expect(bob.tryUpdate({})).equal(false)
+    })
+})
+describe('greetAll', function () {
+    const bob = new Person('bob', 20)
+    const sally = new Person('sally', 25)
+    const arr = [bob, sally]
+    it('should return an array of sayHello called on every object in the input array', function() {
+        expect(Person.greetAll(arr)).to.include('Hello bob!')
+        expect(Person.greetAll(arr)).to.include('Hello sally!')
     })
 })
